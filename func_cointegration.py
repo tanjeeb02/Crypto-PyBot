6b2367c2b348e47d9911a23c6b3c59e57e6e4b2a
tanjeeb02 <tanjeebhossain@gmail.com>
2024-01-09 20:40:08 +0800

    update func_cointegration.py

--- func_cointegration.py
+++ func_cointegration.py
@@ -1,20 +1,21 @@
-
 import math
+
 
 # Put close prices into a list
 def extract_close_prices(prices):
     close_prices = []
-    for price_values in prices:
-        if math.isnan(price_values['close']):
-            return []
-        else:
-            close_prices.append(price_values['close'])
-            print(close_prices)
-            return close_prices
+    if 'result' in prices and 'list' in prices['result']:
+        for item in prices['result']['list']:
+            if math.isnan(float(item[4])):
+                return []
+            else:
+                close_prices.append(float(item[4]))
+    print(close_prices)
+    return close_prices
+
 
 # Calculate cointegrated pairs
 def get_cointegrated_pairs(prices):
-
     # Loop through coins and check for co-integration
     coint_pair_list = []
     included_list = []
@@ -24,10 +25,8 @@ def get_cointegrated_pairs(prices):
         for sym_2 in prices.keys():
             if sym_2 != sym_1:
 
-                print(sym_1, sym_2)
-
                 # Get unique combination id and ensure one off check
-                sorted_characters = sorted([sym_1, sym_2])
+                sorted_characters = sorted(sym_1 + sym_2)
                 unique = ''.join(sorted_characters)
                 if unique in included_list:
                     break
